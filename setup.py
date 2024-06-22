30829c0cef8f45bac28e1bd9a059a1bbd08c06a1
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-06-22 02:29:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tutubo',
-    version='0.0.2a1',
+    version='0.0.2',
     packages=['tutubo'],
     url='https://github.com/OpenJarbas/tutubo',
     license='Apache',
